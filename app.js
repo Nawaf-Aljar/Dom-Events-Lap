00f584840d8697b55c4e4b4b2f1376587e0d6322
Nawaf-Aljar <nawaf.aljar@outlook.com>
2025-11-15 21:18:51 +0300

    improved javascript code

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const buttonElements = document.querySelectorAll(".button")
+const resultElement = document.createElement("p")
+const displayElement = document.querySelector(".display")
+displayElement.appendChild(resultElement)
 num1 = null
 num2 = null
 operator = null
 result = null
-
-const resultElement = document.createElement("p")
-const displayElement = document.querySelector(".display")
-displayElement.appendChild(resultElement)
-
 buttonElements.forEach((button) => {
     button.addEventListener('click', (event) => {
-  
 if(event.target.classList.contains("number")){
     console.log(event.target.innerText);
     if(operator === null && num2 === null){
@@ -19,20 +16,28 @@ if(event.target.classList.contains("number")){
     }
     else if (operator !== null ){
         num2 = event.target.innerText
-    displayElement.textContent = num1 + operator + num2
+    displayElement.textContent = num1 + " " + operator + " "+ num2
     }
 }
  if(event.target.classList.contains("operator")){
     console.log(event.target.innerText);
     if(num1 !== null && num2 === null ){
         operator = event.target.innerText
-        displayElement.textContent = num1 + operator 
-    }
-    
-    
+        displayElement.textContent = num1 + " " + operator 
+    } 
 }
 if(event.target.classList.contains("equals")){
-    if(num1 !== null && operator !== null && num2 !== null && operator === "+"){
+    if (operator !== null){  
+    calculate()  
+    displayElement.textContent = num1 + " " + operator + " " + num2 + " = " + result
+}}
+if (event.target.innerText === ("C")){
+    clear()
+}
+  });
+});
+function calculate(){
+if(num1 !== null && operator !== null && num2 !== null && operator === "+"){
         result = Number(num1) + Number(num2)
     }
     else if (num1 !== null && operator !== null && num2 !== null && operator ==="-"){
@@ -44,15 +49,11 @@ if(event.target.classList.contains("equals")){
     else if (num1 !== null && operator !== null && num2 !== null && operator ==="*"){
         result = Number(num1) * Number(num2)
     }
-        
-    displayElement.textContent = num1 + operator + num2 + "=" + result
 }
-if (event.target.innerText === ("C")){
+function clear(){
     displayElement.textContent = " "
     num1 = null
     num2 = null
     operator = null
     result = null
 }
-  });
-});
