3307a15e96aba4219b5a611d39d8742474a070ba
Nawaf-Aljar <nawaf.aljar@outlook.com>
2025-11-17 15:12:22 +0300

    Fixed two digit + calculatios

--- app.js
+++ app.js
@@ -2,32 +2,32 @@ const buttonElements = document.querySelectorAll(".button")
 const resultElement = document.createElement("p")
 const displayElement = document.querySelector(".display")
 displayElement.appendChild(resultElement)
-num1 = null
-num2 = null
-operator = null
-result = null
+let num1 = " "
+let num2 = " "
+operator = null 
+let result = null
 buttonElements.forEach((button) => {
     button.addEventListener('click', (event) => {
 if(event.target.classList.contains("number")){
     console.log(event.target.innerText);
-    if(operator === null && num2 === null){
-        num1 = event.target.innerText
+    if(operator === null && num2 === " "){
+       num1 += event.target.innerText
     displayElement.textContent = num1
     }
     else if (operator !== null ){
-        num2 = event.target.innerText
+        num2 += event.target.innerText
     displayElement.textContent = num1 + " " + operator + " "+ num2
     }
 }
  if(event.target.classList.contains("operator")){
     console.log(event.target.innerText);
-    if(num1 !== null && num2 === null ){
+    if(num1 !== " " && num2 === " " ){
         operator = event.target.innerText
         displayElement.textContent = num1 + " " + operator 
     } 
 }
 if(event.target.classList.contains("equals")){
-    if (operator !== null){  
+    if (operator !== null ){  
     calculate()  
     displayElement.textContent = num1 + " " + operator + " " + num2 + " = " + result
 }}
@@ -37,23 +37,23 @@ if (event.target.innerText === ("C")){
   });
 });
 function calculate(){
-if(num1 !== null && operator !== null && num2 !== null && operator === "+"){
+if(num1 !== " " && operator !== null && num2 !== " " && operator === "+"){
         result = Number(num1) + Number(num2)
     }
-    else if (num1 !== null && operator !== null && num2 !== null && operator ==="-"){
+    else if (num1 !== " " && operator !== null && num2 !== " " && operator ==="-"){
         result = Number(num1) - Number(num2)
     }
-    else if (num1 !== null && operator !== null && num2 !== null && operator ==="/"){
+    else if (num1 !== " " && operator !== null && num2 !== " " && operator ==="/"){
         result = Number(num1) / Number(num2)
     }
-    else if (num1 !== null && operator !== null && num2 !== null && operator ==="*"){
+    else if (num1 !== " " && operator !== null && num2 !== " " && operator ==="*"){
         result = Number(num1) * Number(num2)
     }
 }
 function clear(){
     displayElement.textContent = " "
-    num1 = null
-    num2 = null
+    num1 = " "
+    num2 = " "
     operator = null
     result = null
 }
